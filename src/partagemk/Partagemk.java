3764d0bb31c898cb9e4cbd1936d28914bcaddf5f
mourisoft <>
2017-10-30 16:47:08 -0400

    ajout system out

--- src/partagemk/Partagemk.java
+++ src/partagemk/Partagemk.java
@@ -17,6 +17,7 @@ public class Partagemk {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Bonjour mon premier partage "); 
+        System.out.println("Bonjour Kerim mon premier partage "); 
     }
     
 }
